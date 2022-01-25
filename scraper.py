3b53bad42885f819009d49f9316977c0724d1510
Dennis <dennis_alexander.vehar@smail.th-koeln.de>
2022-01-25 21:28:28 +0100

    Call via function

--- scraper.py
+++ scraper.py
@@ -1,10 +1,14 @@
-from email import header
-from urllib.request import Request
-import requests
 from bs4 import BeautifulSoup
 import requests
 import json
 import os
+
+
+baseurl = "https://espirs.jrc.ec.europa.eu/en/espirs/public/publicview/" 
+
+cookies_dict = {'.ASPXAUTH' : ""}
+
+
 
 def dump(data:dict, folder:str='tmp', filename:str='dump'):
     if not os.path.isdir(folder):
@@ -12,30 +16,37 @@ def dump(data:dict, folder:str='tmp', filename:str='dump'):
     with open(f"{folder}/{filename}.json", 'w') as file:
         json.dump(data, file, indent=4)
 
-url = "https://espirs.jrc.ec.europa.eu/en/espirs/public/publicview/1e7dc743-971e-400c-b2d6-275a8bb65690" 
+def get_one_object(path:str):
+    
+    url     = baseurl + path
+    r       = requests.get(url=url, cookies=cookies_dict)
+    soup    = BeautifulSoup(r.content, 'html.parser')
+    matches = soup.findAll('div', class_='r')
 
-cookies_dict = {'.ASPXAUTH' : ""}
+    data = dict()
 
-r = requests.get(url=url, cookies=cookies_dict)
+    for match in matches:
+        d = match.find('div', class_='lbl')
+        e = match.find('div', class_='val')
+        if d is None or e is None:
+            continue
+        try:
+            data[d.text] = e.text
+        except:
+            print(f"Ploblemo with: {d}, {e}")
+    
+    return data
 
-soup = BeautifulSoup(r.content, 'html.parser')
+def main():
+    # TODO : Get List of paths
+    paths = ["1e7dc743-971e-400c-b2d6-275a8bb65690"]
 
-#print(soup.prettify)
+    allObjects = list()
 
-matches = soup.findAll('div', class_='r')
+    for path in paths:
+        allObjects.append(get_one_object(path))
 
-data = dict()
+    dump(allObjects)
 
-for match in matches:
-    d = match.find('div', class_='lbl')
-    e = match.find('div', class_='val')
-    if d is None or e is None:
-        continue
-    try:
-        data[d.text] = e.text
-    except:
-        print(f"Ploblemo with: {d}, {e}")
-
-
-dump(data)
-#print(data)
+if __name__ == "__main__":
+    main()
