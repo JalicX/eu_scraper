ca9e4b26af0f03abeaf3b439f5f8040847733e51
Dennis <dennis_alexander.vehar@smail.th-koeln.de>
2022-01-25 20:46:15 +0100

    Can make one call now

--- scraper.py
+++ scraper.py
@@ -1,11 +1,31 @@
 from email import header
 from urllib.request import Request
 import requests
-from pprint import pprint
+from bs4 import BeautifulSoup
+
+
 url = "https://espirs.jrc.ec.europa.eu/en/espirs/public/publicview/1e7dc743-971e-400c-b2d6-275a8bb65690" 
 
 cookies_dict = {'.ASPXAUTH' : ""}
 
 r = requests.get(url=url, cookies=cookies_dict)
 
-pprint(r.text)
+soup = BeautifulSoup(r.content, 'html.parser')
+
+#print(soup.prettify)
+
+matches = soup.findAll('div', class_='r')
+
+data = dict()
+
+for match in matches:
+    d = match.find('div', class_='lbl')
+    e = match.find('div', class_='val')
+    if d is None or e is None:
+        continue
+    try:
+        data[d.text] = e.text
+    except:
+        print(f"Ploblemo with: {d}, {e}")
+
+print(data)
